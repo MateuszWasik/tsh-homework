f9f5b74448727acc3fb23e85b8c8e031843902ac
Mateusz <wasik.matt@gmail.com>
2019-07-30 19:54:35 +0200

    Bug hunt - 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import './assets/scss/app.scss';
 import $ from 'cash-dom';
-
+import fetch from 'isomorphic-fetch';
+require('es6-promise').polyfill();
 
 export class App {
+
   initializeApp() {
     let self = this;
 
@@ -10,20 +12,18 @@ export class App {
       let userName = $('.username.input').val();
 
       fetch('https://api.github.com/users/' + userName)
-        .then((response)=> {response.json})
-        .then(function (body) {
+        .then(response => response.json())
+        .then((body) => {
           self.profile = body;
           self.update_profile();
         })
-
     })
-
   }
 
   update_profile() {
-    $('#profile-name').text($('.username.input').val())
-    $('#profile-image').attr('src', this.profile.avatar_url)
-    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login)
-    $('#profile-bio').text(this.profile.bio || '(no information)')
+    $('#profile-name').text($('.username.input').val());
+    $('#profile-image').attr('src', this.profile.avatar_url);
+    $('#profile-url').attr('href', this.profile.html_url).text(this.profile.login);
+    $('#profile-bio').text(this.profile.bio || '(no information)');
   }
 }
